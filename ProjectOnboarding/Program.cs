51b2a6ad7eaa4a6fc0722636ff01a10e25fb7e65
Simran MAnandhar <simran.manandhar@revature.net>
2021-11-03 11:33:56 -0400

    file for first coding challenge

---
+++ ProjectOnboarding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectOnboarding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
